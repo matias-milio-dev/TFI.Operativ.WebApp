7a7b2651b59321785ac6c8ae3042ee46bcb52486
Matias Milio <MatiasNicolas.Milio@alumnos.uai.edu.com>
2026-08-20 11:06:03 -0300

    refactor: centraliza manejo de mensajes en Notificaciones.ascx.cs
    El control ahora sabe traducir excepciones y tipos de error a mensajes,
    y resolver textos de exito por clave de recurso, absorbiendo logica que
    antes se repetia en cada code-behind. PaginaSeguraBase expone
    ControlNotificaciones para llegar al control sin repetir el cast al Master.

--- Operativ.Web/Paginas/Controles/Notificaciones.ascx.cs
+++ Operativ.Web/Paginas/Controles/Notificaciones.ascx.cs
@@ -1,10 +1,37 @@
+using System;
 using System.Text.RegularExpressions;
 using System.Web.UI;
+using Operativ.BE.Enums;
+using Operativ.BE.Errores;
 
 namespace Operativ.Web.Controles;
 public partial class Notificaciones : UserControl
 {
     private static readonly Regex PrefijoCodigoError = new Regex(@"^ERR\d+\s*-\s*");
+
+    private readonly ErroresHandler erroresHandler = new ErroresHandler();
+
+    public void MostrarMensaje(Exception excepcion)
+    {
+        OperativException excepcionOperativ = erroresHandler.TraducirExcepcion(excepcion);
+        MostrarMensaje(erroresHandler.GetMensaje(excepcionOperativ));
+    }
+
+    public void MostrarMensaje(TipoError tipoError)
+    {
+        MostrarMensaje(erroresHandler.GetMensaje(tipoError));
+    }
+
+    public void MostrarMensaje(TipoError tipoError, string[] parametros)
+    {
+        MostrarMensaje(erroresHandler.GetMensaje(tipoError, parametros));
+    }
+
+    public void MostrarExito(string claveRecurso)
+    {
+        string mensaje = (string)GetGlobalResourceObject("Textos", claveRecurso);
+        MostrarMensaje(mensaje, true);
+    }
 
     public void MostrarMensaje(string mensaje)
     {
